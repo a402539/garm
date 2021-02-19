0ce3422f8f3cfbe96917da8fad16be84acd34713
Serg <salekseev@mvs.group>
2021-02-19 20:42:35 +0300

    update

--- src/worker/dataManager.js
+++ src/worker/dataManager.js
@@ -23,76 +23,93 @@ async function getBoxTiles(signal, bbox) {
 	return response.json();
 }
 
+function getTilePromise ({layerId, z, x, y}) {
+	return fetch(`/tile/${layerId}/${z}/${x}/${y}`)
+	.then(res => res.blob())
+	.then(blob => blob.arrayBuffer())
+	.then(buf => {				
+		const t = {};
+		const {layers} = new VectorTile(new Protobuf(buf));								
+		Object.keys(layers).forEach(k => {
+			const layer = layers[k];
+			t[k] = { features: [], x, y, z, extent: layer.extent, layerId };
+			for (let i = 0; i < layer.length; ++i) {
+				const vf = layer.feature(i);							
+				const coordinates = vf.loadGeometry();
+				const path = new Path2D();
+				coordinates[0].forEach((p, i) => {
+					if (i) {
+						path.lineTo(p.x, p.y);
+					}
+					else {
+						path.moveTo(p.x, p.y);
+					}
+				});
+				t[k].features.push({type: vf.type, path});							
+			}					
+		});				
+		return t;
+	});
+
+}
+
 async function getTiles (zoom, bbox, bounds) {	
 	if (abortController) {
 		abortController.abort();
 	}
 	abortController = new AbortController();	
 	const items = await getBoxTiles(abortController.signal, bbox);	
-
-	const ctx = canvas.getContext("2d");
-	ctx.resetTransform();
-	ctx.clearRect(0, 0, canvas.width, canvas.height);	
-
-	Promise.all(
-		items.map(({layerId, x, y, z}) => {
-			return fetch(`/tile/${layerId}/${z}/${x}/${y}`)
-			.then(res => res.blob())
-			.then(blob => blob.arrayBuffer())
-			.then(buf => {				
-				const t = {};
-				const {layers} = new VectorTile(new Protobuf(buf));								
+	const promisesByLayers = {};
+	items.forEach(it => {
+		let data = promisesByLayers[it.layerId] || {};
+		let arr = data.promises || [];
+		arr.push(getTilePromise(it));
+		data.promises = arr;
+		let tiles = data.tiles || [];
+		tiles.push(it);
+		data.tiles = tiles;
+		promisesByLayers[it.layerId] = data;
+	});
+	Object.keys(promisesByLayers).forEach(layerId => {
+		const pt = promisesByLayers[layerId];
+		const promises = pt.promises;
+		const ctx = canvas.getContext("2d");
+		ctx.resetTransform();
+		ctx.clearRect(0, 0, canvas.width, canvas.height);	
+	
+		Promise.all(promises)
+		.then(tiles => {		
+			tiles.forEach(layers => {
 				Object.keys(layers).forEach(k => {
-					const layer = layers[k];
-					t[k] = { features: [], x, y, z, extent: layer.extent };
-					for (let i = 0; i < layer.length; ++i) {
-						const vf = layer.feature(i);							
-						const coordinates = vf.loadGeometry();
-						const path = new Path2D();
-						coordinates[0].forEach((p, i) => {
-							if (i) {
-								path.lineTo(p.x, p.y);
-							}
-							else {
-								path.moveTo(p.x, p.y);
-							}
-						});
-						t[k].features.push({type: vf.type, path});							
-					}					
-				});				
-				return t;
-			});
-		})
-	)
-	.then(tiles => {		
-		tiles.forEach(layers => {
-			Object.keys(layers).forEach(k => {
-				const {features, x, y, z, extent} = layers[k];				
-				const tw = 1 << (8 + zoom - z);
-				let x0 = x * tw - bounds.min.x;
-				if (x0 + tw < 0) {
-					x0 += Math.pow(2, z) * tw;
-				}
-				const y0 = y * tw - bounds.min.y;
-				ctx.resetTransform();
-				const sc = tw / extent;				
-				ctx.transform(sc, 0, 0, sc, x0, y0);
-				features.forEach(feature => {
-					if (feature.type === 3) {															
-						Renderer.renderPath(ctx, feature.path);
+					const {features, x, y, z, extent} = layers[k];				
+					const tw = 1 << (8 + zoom - z);
+					let x0 = x * tw - bounds.min.x;
+					if (x0 + tw < 0) {
+						x0 += Math.pow(2, z) * tw;
 					}
+					const y0 = y * tw - bounds.min.y;
+					ctx.resetTransform();
+					const sc = tw / extent;				
+					ctx.transform(sc, 0, 0, sc, x0, y0);
+					features.forEach(feature => {
+						if (feature.type === 3) {															
+							Renderer.renderPath(ctx, feature.path);
+						}
+					});
 				});
-			});
-		});		
-		bitmapToMain(canvas);
-	})
-	.catch(() => {});
+			});		
+			bitmapToMain(layerId, canvas);
+		})
+		.catch(() => {});
+		
+	});
 }
 
-const bitmapToMain = canvas => {
+const bitmapToMain = (layerId, canvas) => {
 	var imageData = canvas.transferToImageBitmap();
 	self.postMessage({
 		cmd: 'rendered',
+		layerId,
 		bitmap: imageData
 	}, [ imageData ]);
 };
